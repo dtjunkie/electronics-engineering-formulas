6e45fd06cd3e2e4734bfa583af5543bbbdee29c4
Daniel Joseph T. Castro <90781857+dtjunkie@users.noreply.github.com>
2021-11-06 10:21:38 +0800

    Version 1.1
    Renamed as "Maximum number of electrons in a shell.py".

--- Maximum number of electrons in a shell.py
+++ Maximum number of electrons in a shell.py
@@ -2,8 +2,11 @@ def max_number_electrons(n):
     n_electrons = 2 * (n ** 2)
     print(str(n_electrons)) # returns the maximum nunmber of electrons in the n shell
     
-    
+"""
+
 where:
 
 n_electrons = Maximum number of electrons in the n shell
 n = The number of the shell
+
+"""
