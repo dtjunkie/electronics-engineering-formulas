bb56c29a9f96550dd3bf8ec6b6fdd1f140eee0f7
Daniel Joseph T. Castro <90781857+dtjunkie@users.noreply.github.com>
2021-11-06 10:28:24 +0800

    Version 1.1
    Renamed as "zener_impedance.py".

--- zener_impedance.py
+++ zener_impedance.py
@@ -4,9 +4,13 @@ def zener_impedance(delv_z, deli_z):
     
     r_zener = delv_z/deli_z
     print(str(r_zener))
+
+"""    
     
 where:
 
 r_zener = Zener impedance, ohms
 delv_z = Change in the zener breakdown voltage, V
 deli_z = Change in the zener current, A
+
+"""
