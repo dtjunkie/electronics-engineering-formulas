5e75821a037c3ef01cf5fa95c690d509749f0d5e
Daniel Joseph T. Castro <90781857+dtjunkie@users.noreply.github.com>
2021-11-15 15:20:39 +0800

    Version 2.0
    - Added an __init__() function
    - Added "model" to the parameters
    - Removed all the comments and replaced with a single docstring

--- diode_approximation.py
+++ diode_approximation.py
@@ -1,49 +1,57 @@
 class Diode_approximations:
     
-    def ideal_model_fwd(v_bias, r_limit):
-        i_fwd = v_bias / r_limit
-        v_rlimit = i_fwd * r_limit
-        print(str(i_fwd) + ", " + str(0) + ", " + str(v_rlimit))
-        #returns the values of the forward current, forward voltage, and the voltage across the limit resistor of the ideal diode
+    """returns the values of the bias current, bias voltage, and the 
+    voltage across the limit resistor of the diode models in different
+    operating conditions or biases"""
+    
+    def __init__(self, model, v_bias, r_limit, r_dyn=10, i_rev=1e-06):
+        self.model = model 
+        self.v_bias = v_bias
+        self.r_limit = r_limit
+        self.r_dyn = r_dyn
+        self.i_rev = i_rev
         
-    def practical_model_fwd(v_bias, r_limit):
-        i_fwd = (v_bias - 0.7) / r_limit
-        v_rlimit = i_fwd * r_limit
-        print(str(i_fwd) + ", " + str(0.7) + ", " + str(v_rlimit))
-        #returns the values of the forward current, forward voltage, and the voltage across the limit resistor of the practical diode
+    def forward_bias(self):
+        if self.model == "ideal":
+            i_fwd = self.v_bias / self.r_limit
+            v_rlimit = i_fwd * self.r_limit
+            print(str(i_fwd) + ", " + str(0) + ", " + str(v_rlimit))
+        elif self.model == "practical":
+            i_fwd = (self.v_bias - 0.7) / self.r_limit
+            v_rlimit = i_fwd * self.r_limit
+            print(str(i_fwd) + ", " + str(0.7) + ", " + str(v_rlimit))
+        elif self.model == "complete":
+            i_fwd = (self.v_bias - 0.7) / (self.r_limit + self.r_dyn)
+            v_fwd = 0.7 + (i_fwd * self.r_dyn)
+            v_rlimit = i_fwd * self.r_limit
+            print(str(i_fwd) + ", " + str(v_fwd) + ", " + str(v_rlimit))
+        else:
+            return ValueError("Invalid model type.")
         
-    def complete_model_fwd(v_bias, r_limit, r_dyn=10):
-        i_fwd = (v_bias - 0.7) / (r_limit + r_dyn)
-        v_fwd = 0.7 + (i_fwd * r_dyn)
-        v_rlimit = i_fwd * r_limit
-        print(str(i_fwd) + ", " + str(v_fwd) + ", " + str(v_rlimit))
-        #returns the values of the forward current, forward voltage, and the voltage across the limit resistor of the complete diode
-        
-    def ideal_model_rev(v_bias, r_limit):
-        print(str(0) + ", " + str(v_bias) + ", " + str(0))
-        #returns the values of the reverse current, reverse voltage, and the voltage across the limit resistor of the ideal diode
-        
-    def practical_model_rev(v_bias, r_limit):
-        print(str(0) + ", " + str(v_rev) + ", " + str(0))
-        #returns the values of the reverse current, reverse voltage, and the voltage across the limit resistor of the practical diode
-        
-    def complete_model_rev(v_bias, r_limit, i_rev=1e-06):
-        v_rlimit = i_rev * r_limit
-        v_rev = v_bias - v_rlimit
-        print(str(i_rev) + ", " + str(v_rev) + ", " + str(v_rlimit)) 
-        #returns the values of the reverse current, reverse voltage, and the voltage across the limit resistor of the complete diode
+    def reverse_bias(self):
+        if self.model == "ideal":
+            print(str(0) + ", " + str(self.v_bias) + ", " + str(0))
+        elif self.model == "practical":
+            print(str(0) + ", " + str(self.v_bias) + ", " + str(0))
+        elif self.model == "complete":
+            v_rlimit = self.i_rev * self.r_limit
+            v_rev = self.v_bias - v_rlimit
+            print(str(self.i_rev) + ", " + str(v_rev) + ", " + str(v_rlimit)) 
+        else:
+            return ValueError("Invalid model type.")  
 
 """        
         
 where:
 
-i_fwd = Forward current, A
+model = Diode model ("ideal", "practical", or "complete")
 v_bias = Bias voltage, V
 r_limit = Limit resistance, ohms
+r_dyn = Forward dynamic resistance, ohms
+i_rev = Reverse current, V (0 V for ideal and practical models)
+i_fwd = Forward current, A
 v_rlimit = Voltage across the limiting resistor, V
 v_fwd = Forward voltage, V (0 V for ideal model and 0.7 V for practical and complete models)
-r_dyn = Forward dynamic resistance, ohms
 v_rev = Reverse voltage, V (v_rev = v_bias for ideal and practical models)
-i_rev = Reverse current, V (0 V for ideal and practical models)
 
 """
